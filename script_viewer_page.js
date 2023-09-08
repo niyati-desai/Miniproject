9d5e71495dab3c8a7ed8e005acf79448ce9f1fb8
COMP-KJSIT2023 <126879674+COMP-KJSIT2023@users.noreply.github.com>
2023-09-08 11:05:33 +0530

    Add files via upload

--- script_viewer_page.js
+++
@@ -1,9 +0,0 @@
-// Get references to the elements
-const welcomeText = document.getElementById("welcome-text");
-const optionsContainer = document.getElementById("options-container");
-
-// Toggle the options when clicking on the welcome text
-welcomeText.addEventListener("click", () => {
-    // Toggle the class that controls the height and overflow
-    optionsContainer.classList.toggle("hidden-options");
-});
